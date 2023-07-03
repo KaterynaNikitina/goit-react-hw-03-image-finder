86b8941af0be8845ec06192b5fd7224335dc0eca
Katerina <Kateryna.e.nikitina@gmail.com>
2023-07-02 18:47:03 -0600

    dashboard fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import { Dashboard } from 'components/Dashboard/Dashboard';
+// import { Dashboard } from 'components/Dashboard/Dashboard';
+import { Dashboard } from "./Dashboard/Dashboard";
 
 export const App = () => {
   return <Dashboard/>;
