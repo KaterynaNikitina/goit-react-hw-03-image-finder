db6c77aff2887aae41a49c39e693286cc0c85f7d
Katerina <Kateryna.e.nikitina@gmail.com>
2023-07-02 18:05:14 -0600

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import {Dashboard} from 'components/Dashboard/Dashboard';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <Dashboard/>;
 };
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
