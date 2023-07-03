00b726f5bfd9cef2bcb0386087d1211a83f3974c
Katerina <Kateryna.e.nikitina@gmail.com>
2023-07-02 18:34:31 -0600

    adjusted

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,6 @@
-import {Dashboard} from 'components/Dashboard/Dashboard';
+import { Dashboard } from 'components/Dashboard/Dashboard';
 
 export const App = () => {
   return <Dashboard/>;
 };
 
-
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
